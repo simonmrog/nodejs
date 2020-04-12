721d0d8a933b95b04303c695b1bf9424a09ef8f7
simonmrog <simonmrog@gmail.com>
2020-04-12 16:52:32 -0500

    fixed bug in api-rest-basics and login

--- api-rest-basics/controllers/product.js
+++ api-rest-basics/controllers/product.js
@@ -6,18 +6,30 @@ const getProduct = (req, res) => {
   console.log("[INFO] GET /api/products");
   let productId = req.params.productId;
   Product.findById(productId, (err, product) => {
-    if (err) return res.status(500).send({ message: "Internal Server Error" });
-    else if (!product) return res.status(404).send({ message: "Product Not Found" });
-    res.status(200).send({ product });
+    if (err) return res.status(500).send({
+      statsu: "error",
+      message: "Internal Server Error"
+    });
+    else if (!product) return res.status(404).send({
+      status: "error", 
+      message: "Product Not Found"
+    });
+    res.status(200).send({ status: "ok", product });
   });
 };
 
 const getProducts = (req, res) => {
   console.log("[INFO] GET /api/product");
   Product.find({}, (err, products) => {
-    if (err) return res.status(500).send({ message: "Internal Server Error" });
-    if (!products) return res.status(404).send( { message: "No Products Found" });
-    res.status(200).send({ products });
+    if (err) return res.status(500).send({
+      status: "error",
+      message: "Internal Server Error"
+    });
+    if (!products) return res.status(404).send({
+      status: "error",
+      message: "No Products Found"
+    });
+    res.status(200).send({ status: "ok", products });
   });
 };
 
@@ -31,8 +43,15 @@ const saveProduct = (req, res) => {
     description: req.body.description
   });
   product.save((err, productStored) => {
-    if (err) return req.status(500).send({ message: `Error saving the product to DB: ${err}` });
-    res.status(200).send({ message: "Product Received", product: productStored })
+    if (err) return req.status(500).send({
+      status: "error",
+      message: `Error saving the product to DB: ${err}`
+    });
+    res.status(200).send({
+      status: "ok",
+      message: "Product Received",
+      product: productStored
+    })
   });
 };
 
@@ -41,9 +60,15 @@ const updateProduct = (req, res) => {
   let productId = req.params.productId;
   let update = req.body;
   Product.findByIdAndUpdate(productId, update, (err, updatedProduct) => {
-    if (err) return res.status(500).send({ message: "Internal Server Error" });
-    else if (!updatedProduct) return res.status(404).send({ message: "Product Not Found" });
-    res.status(200).send({ product: updatedProduct });
+    if (err) return res.status(500).send({
+      status: "ok",
+      message: "Internal Server Error"
+    });
+    else if (!updatedProduct) return res.status(404).send({
+      status: "error",
+      message: "Product Not Found"
+    });
+    res.status(200).send({ status: "ok", product: updatedProduct });
   });
 };
 
@@ -51,11 +76,23 @@ const deleteProduct = (req, res) => {
   console.log("[INFO] DELETE /api/product");
   let productId = req.params.productId;
   Product.findById(productId, (err, product) => {
-    if (err) return res.status(500).send({ message: "Internal Server Error" });
-    else if (!product) return res.status(404).send({ message: "Product Not Found" });
+    if (err) return res.status(500).send({
+      status: "error",
+      message: "Internal Server Error"
+    });
+    else if (!product) return res.status(404).send({
+      status: "error",
+      message: "Product Not Found"
+    });
     product.remove(err => {
-      if (err) return res.status(500).send({ message: "Error Deleting Product" });
-    res.status(200).send({ message: "Product Deleted" });
+      if (err) return res.status(500).send({
+        status: "error",
+        message: "Error Deleting Product"
+      });
+      res.status(200).send({
+        status: "ok",
+        message: "Product Deleted"
+      });
     })
   });
 };
--- api-rest-basics/middlewares/auth.js
+++ api-rest-basics/middlewares/auth.js
@@ -5,19 +5,20 @@ const services = require("../services");
 const isAuth = async (req, res, next) => {
 	try {
 		// bearer authentication: Bearer <token>
-		let token = req.headers.authorization.split(" ")[1];
-		console.log(token);
-		if (!token) throw ({
+		const auth = req.headers.authorization;
+		if (!auth) throw ({ message: "Authorization required" });
+		const token = auth.split(" ")[1];
+		if (!token || token == "undefined") throw ({
 			code: 403,
 			status: "access denied",
 			message: "Token required"
-		});	
+		});
 		req.userId = await services.decodeToken(token);
 		next();
 	} catch(err) {
 			res.status(err.code || 500).send({
 				status: err.status || "error",
-				message: err.message || ""
+				message: err.message || err
 			});
 	};
 };
