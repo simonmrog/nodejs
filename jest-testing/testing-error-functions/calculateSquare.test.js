93c274cb9934fd41fec288922c379babe4aa2a6c
Simón Murillo Gómez <simonmrog@Simons-MacBook-Air.local>
2022-10-25 18:48:33 -0500

    fixing tests for error functions -> wrap the calls inside functions

--- jest-testing/testing-error-functions/calculateSquare.test.js
+++ jest-testing/testing-error-functions/calculateSquare.test.js
@@ -1,9 +1,18 @@
-const { calculateSquare } = require("./calculateSquare")
+const { calculateSquare } = require("./calculateSquare");
 
 test("Should throw an error if called without an argument", () => {
-  expect(calculateSquare()).toThrow("You must provide a number");
+  expect(() => {
+    calculateSquare();
+  }).toThrow("You must provide a number");
 });
 
 test("Should throw an error if called without a number", () => {
-  expect(calculateSquare("45")).toThrow("You must provide a number");
+  expect(() => {
+    calculateSquare("45");
+  }).toThrow("You must provide a number");
+});
+
+test("Shoud return the square of the number", () => {
+  const result = calculateSquare(5);
+  expect(result).toBe(25);
 });
