8d35e4f41df7aab72e9c2d2a4d62c6eac52c490d
Simón Murillo Gómez <simonmrog@Simons-MacBook-Air.local>
2023-01-06 18:20:12 -0500

    feature: adding main routes

--- sequalize-rest-api/src/app.js
+++ sequalize-rest-api/src/app.js
@@ -1,5 +1,9 @@
 import express from "express";
+import projectsRoutes from "./routes/projects.routes.js";
+import tasksRoutes from "./routes/tasks.routes.js";
 
 const app = express();
 
+app.use("/api/projects", projectsRoutes);
+app.use("/api/tasks", tasksRoutes);
 export default app;
---
+++ sequalize-rest-api/src/routes/projects.routes.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.get("");
+router.post("");
+router.put("/:id");
+router.delete("/:id");
+router.get("/:id");
+
+export default router;
