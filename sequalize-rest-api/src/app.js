f98ecdd8cafc7fcda605559fb4308106e9f25bac
Simón Murillo Gómez <simonmrog@Simons-MacBook-Air.local>
2023-01-07 12:34:35 -0500

    feature: error middleware added

--- sequalize-rest-api/src/app.js
+++ sequalize-rest-api/src/app.js
@@ -1,9 +1,21 @@
 import express from "express";
 import projectsRoutes from "./routes/projects.routes.js";
 import tasksRoutes from "./routes/tasks.routes.js";
+import { errorHandler } from "./middlewares/error.js";
 
 const app = express();
 
+// Middlewares
+app.use(express.json());
+
+// Routes
 app.use("/api/projects", projectsRoutes);
 app.use("/api/tasks", tasksRoutes);
+app.use("*", (req, res) => res.status(404).json({
+  message: "Route not found"
+}));
+
+// Error middleware
+app.use(errorHandler);
+
 export default app;
