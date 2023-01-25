7f22ee3d70e143a150927cffbf1f251ae8142902
Simón Murillo Gómez <simonmrog@Simons-MacBook-Air.local>
2023-01-25 10:35:52 -0500

    fix: fixing typo in config

--- flights-api/src/config.js
+++ flights-api/src/config.js
@@ -4,6 +4,6 @@ module.exports = {
   MONGODB_HOST: process.env.MONGODB_HOST,
   MONGODB_PORT: process.env.MONGODB_PORT,
   MONGODB_USER: process.env.MONGODB_USER,
-  MONGODB_PASSWORD: process.env.MONDODB_PASSWORD,
+  MONGODB_PASSWORD: process.env.MONGODB_PASSWORD,
   MONGODB_NAME: process.env.MONGODB_NAME,
 };
