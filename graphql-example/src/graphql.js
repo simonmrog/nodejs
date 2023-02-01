e0b2dc36ede10e2b2a41a09cc5af90c7350dcaed
Simón Murillo Gómez <simonmrog@Simons-MacBook-Air.local>
2023-01-31 23:21:11 -0500

    getting data from jsonfile

--- graphql-example/src/graphql.js
+++ graphql-example/src/graphql.js
@@ -5,27 +5,44 @@ const { graphql, buildSchema } = pkg;
 import expressGraphQL from "express-graphql";
 const { graphqlHTTP } = expressGraphQL;
 
+// Data
+import data from "./data.json" assert { type: "json" };
+const { books } = data;
 
 const schema = buildSchema(`
   type Query {
-    hello: String
+    book(id: Int!): Book
+    books(topic: String): [Book]
+  }
+
+  type Book {
+    id: Int
+    title: String
+    author: String
+    topic: String
+    url: String
   }
 `);
 
-const resolvers = {
-  hello: function () {
-    return "Hello World"
-  }
-}
+const getBookById = (args) => {
+  const id = args.id;
+  return books.find((book) => book.id == id);
+};
 
-graphql(schema, `{ hello }`, resolvers).then(function (data) {
-  console.log(data);
-});
+const getBooks = () => {
+  const filteredBooks = books.filter((book) => book.id == id);
+  if (filteredBooks.length === 0) return books;
+};
+
+const resolvers = {
+  book: getBookById,
+  books: getBooks,
+};
 
 const gqlMiddleware = graphqlHTTP({
   schema,
   rootValue: resolvers,
-  graphiql: true
+  graphiql: true,
 });
 
 export default gqlMiddleware;
